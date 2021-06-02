4907c24500bf31d5eb784689264dafa2822f26c0
Hu Jindi <hujindi1212@gmail.com>
2021-06-02 13:13:31 +0800

    golang tutorial

--- hello/hello.go
+++ hello/hello.go
@@ -10,11 +10,12 @@ import (
 func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
-	message, err := greetings.Hello("")
+	names := []string{"Gladys", "Samantha", "Darrin"}
+	messages, err := greetings.Hellos(names)
 	if err != nil {
 		log.Fatal(err)
 	}
 	// Get a greeting message and print it.
 	// message := greetings.Hello("Gladys")
-	fmt.Println(message)
+	fmt.Println(messages)
 }
